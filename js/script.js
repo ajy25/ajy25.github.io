327abb7047b0b15f16c189952e2d1f3131d03672
ajy25 <andrew_j_yang@brown.edu>
2023-11-26 16:21:47 -0600

    some initial progress

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if (document.getElementById('project-link')) {
+  document.getElementById('project-link').addEventListener('click', () => {
+    document.getElementById('project-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+
+if (document.getElementById('research-link')) {
+  document.getElementById('research-link').addEventListener('click', () => {
+    document.getElementById('research-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+
